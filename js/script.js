3034d15bbca2ba7d6299a00e7abaaccf86e89ce5
shinik332 <shinik332@gmail.com>
2024-01-18 16:07:44 +0800

    membuat website cafe kopi kenangan

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// TOGGLE CLASS ACTIVE
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// KLIK DI LUAR SIDEBAR UNTUK MENGHILANGKAN NAV
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
